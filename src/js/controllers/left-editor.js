5c37c5dbc25c8b49d8edb292060fff5f98c8ac91
Chris Coralli <ccoralli@gmail.com>
2017-03-30 14:30:44 -0400

    wired up clipart selectors to backend.

--- src/js/controllers/left-editor.js
+++ src/js/controllers/left-editor.js
@@ -1,7 +1,47 @@
-function LeftSandboxController () {
+function LeftSandboxController ($http, SERVER) {
+    let vm = this;
+    vm.showCategory = showCategory;
+    vm.emptyCatData = emptyCatData;
+    vm.catData = null;
+    vm.categories = {
+        shapes: 'Shapes/Symbols',
+        letters: 'Letters/Numbers',
+        emojis: 'Emojis',
+        nature: 'Nature',
+        parties: 'Parties/Events',
+        occupations: 'Occupations',
+        music: 'Music',
+        greekLife: 'Greek Life',
+        charity: 'Charity',
+        religion: 'Religion',
+        sports: 'Sports/Games',
+        animals: 'Animals',
+        mascots: 'Mascots',
+        america: 'America',
+        military: 'Military',
+        colleges: 'Colleges',
+        transportation: 'Transportation',
+        school: 'School',
+        people: 'People',
+        food: 'Food/Drink'
+    };
 
+    function showCategory (name) {
+        $http.get(`${SERVER}/cliparts?category=${name}`)
+            .then(resp =>   {
+                console.log(resp);
+                vm.catData = resp.data;
+                console.log(vm.catData);
+
+            })
+
+    }
+    function emptyCatData() {
+        vm.catData = null;
+        console.log(vm.catData);
+    }
 }
 
-LeftSandboxController.$inject = [];
+LeftSandboxController.$inject = ['$http', 'SERVER'];
 
 export default LeftSandboxController;
