771e94d5edeb0a9e8651667a55d71202917d1501
Artem Aleksandruk <artem.alek@icloud.com>
2017-03-31 16:00:28 -0400

    swatches working and wired to change tshirt url of the sandbox.

--- src/js/controllers/right-editor.js
+++ src/js/controllers/right-editor.js
@@ -1,7 +1,72 @@
-function RightSandboxController () {
+function RightSandboxController ($scope) {
 
+  let vm = this;
+  vm.tshirtColor = {
+    white: {
+      url: './images/tshirts/White Front T-Shirt-450x550.png',
+      class: 'white',
+      id: 'white'
+    },
+    sportsGrey: {
+      url: './images/tshirts/Sport Grey Front T-Shirt-450x550.png',
+      class: 'grey',
+      id: 'sports-grey'
+    },
+    black: {
+      url: 'https://skpsoft.com/baby/wp-content/uploads/2016/09/default-thumbnail.jpg',
+      class: 'black',
+      id: 'black'
+    },
+    darkHeather: {
+      url: '.https://skpsoft.com/baby/wp-content/uploads/2016/09/default-thumbnail.jpg',
+      class: 'blue-grey',
+      id: 'dark-heather'
+    },
+    royal: {
+      url: 'https://skpsoft.com/baby/wp-content/uploads/2016/09/default-thumbnail.jpg',
+      class: 'light-blue darken-4',
+      id: 'royal'
+    },
+    skyBlue: {
+      url: 'https://skpsoft.com/baby/wp-content/uploads/2016/09/default-thumbnail.jpg',
+      class: 'blue lighten-3',
+      id: 'sky-blue'
+    },
+    floBlue: {
+      url: 'https://skpsoft.com/baby/wp-content/uploads/2016/09/default-thumbnail.jpg',
+      class: 'blue accent-1',
+      id: 'flo-blue'
+    },
+    seafoam: {
+      url: 'https://skpsoft.com/baby/wp-content/uploads/2016/09/default-thumbnail.jpg',
+      class: 'cyan darken-3',
+      id: 'sports-grey'
+    },
+    neonPink: {
+      url: 'https://skpsoft.com/baby/wp-content/uploads/2016/09/default-thumbnail.jpg',
+      class: 'pink lighten-2',
+      id: 'sports-grey'
+    },
+    neonGreen: {
+      url: 'https://skpsoft.com/baby/wp-content/uploads/2016/09/default-thumbnail.jpg',
+      class: 'lime accent-1',
+      id: 'neon-green'
+    },
+    neonOrange: {
+      url: 'https://skpsoft.com/baby/wp-content/uploads/2016/09/default-thumbnail.jpg',
+      class: 'orange accent-4',
+      id: 'neon-orange'
+    },
+  };
+
+
+  vm.switchTshirt = switchTshirt;
+
+  function switchTshirt (url) {
+    $scope.$emit('tshirtUrl', url);
+  }
 }
 
-RightSandboxController.$inject = [];
+RightSandboxController.$inject = ['$scope'];
 
 export default RightSandboxController;
--- src/js/controllers/tshirt-editor.js
+++ src/js/controllers/tshirt-editor.js
@@ -1,10 +1,15 @@
 function TshirtEditorController ($scope) {
   let vm = this;
   vm.images = [];
+  vm.tshirtUrl= './images/tshirts/White Front T-Shirt-450x550.png';
 
   $scope.$on('image', (event, url) =>  {
     vm.images.push(url);
-  } )
+  });
+
+  $scope.$on('tshirtUrl', (event, url) => {
+    vm.tshirtUrl = url;
+  });
 }
 
 TshirtEditorController.$inject = ['$scope'];
