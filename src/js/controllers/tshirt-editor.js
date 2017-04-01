a4a572baf28d9794678fea3478f803d9ba29ff4a
Artem Aleksandruk <artem.alek@icloud.com>
2017-04-01 12:16:01 -0400

    complete wire of tshirt url info

--- src/js/controllers/tshirt-editor.js
+++ src/js/controllers/tshirt-editor.js
@@ -1,5 +1,13 @@
 function TshirtEditorController ($scope) {
   let vm = this;
+  vm.projectInfo = {
+    name: '',
+    color: '',
+    ts_front_url: '',
+    ts_back_url: '',
+    tsFrontImages: []
+  };
+
   vm.images = [];
   vm.tshirtUrl= './images/tshirts/White Front T-Shirt-450x550.png';
   vm.texts = [];
@@ -8,8 +16,13 @@ function TshirtEditorController ($scope) {
     vm.images.push(url);
   });
 
-  $scope.$on('tshirtUrl', (event, url) => {
-    vm.tshirtUrl = url;
+  $scope.$on('tshirtUrl', (event, data) => {
+    vm.tshirtUrl = data.url;
+    vm.projectInfo.name = data.id;
+    vm.projectInfo.color = data.id;
+    vm.projectInfo.ts_front_url = data.url;
+    vm.projectInfo.ts_back_url = data.url;
+    console.log(vm.projectInfo);
   });
 
   $scope.$on('addText', (event, text) => {
